c5b05115f58b1fb7fe29015148c644efde629ce8
christhebutcher <ccwijnia@gmail.com>
2015-11-20 17:07:45 +0100

    gulp sprites

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sprite = require('gulp-sprite');
+ 
+gulp.task('sprites', function () {
+    gulp.src('./src/img/*.png')
+      .pipe(sprite('sprites.png', {
+        imagePath: 'dist/img',
+        cssPath: './src/scss/utils/',
+        preprocessor: 'scss'
+      }))
+      .pipe(gulp.dest('./dist/img/'));
+});
